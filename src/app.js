00024b940ac1310cc6a4e5d28f657e2e5b946f36
Begona Fernandez <boadicea1979@yahoo.es>
2019-06-17 17:49:19 +0100

    Added React form and a axios post to create new votes via frontend

--- src/app.js
+++ src/app.js
@@ -9,21 +9,73 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
+      data: {
+        email: '',
+        candidate: ''
+      }
     }
-
+    this.handleChange  = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount(){
     axios.get('/api/vote/')
       .then( res =>{
-        this.setState({ user: res.data})
+        this.setState({ vote: res.data})
       })
       .catch(err =>this.setState({error: err.message}))
   }
 
+  handleChange({target: {name, value}}){
+    const data = {...this.state.data, [name]: value }
+    const errors = {...this.state.errors, [name]: null }
+    this.setState({ data, errors })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    axios
+      .post('/api/vote',
+        this.state.data)
+      .then(this.setState({data: {'candidate': '', 'email': ''}}))
+      .catch(err => this.setState({ error: err.response.data.message }))
+  }
+
   render(){
+    if(!this.state) return <h1>Loading...</h1>
     return(
-      <h1>Hello</h1>
+      <section className="section">
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-primary"
+                  type="text"
+                  placeholder="Email address"
+                  name="email"
+                  value={this.state.data.email }
+                  onChange={this.handleChange}>
+                </input>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-primary"
+                  type="text"
+                  placeholder="Candidate Number"
+                  name="candidate"
+                  value={this.state.data.candidate}
+                  onChange={this.handleChange}>
+                </input>
+              </div>
+            </div>
+            <button  className='button is-primary'>Submit</button>
+          </form>
+        </div>
+      </section>
     )
   }
 }
