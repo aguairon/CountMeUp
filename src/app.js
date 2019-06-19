d5416a34a6514dc3920173ed4c6a2bbaf77d2359
Begona Fernandez <boadicea1979@yahoo.es>
2019-06-19 11:31:09 +0100

    Extracted the form into its own component

--- src/app.js
+++ src/app.js
@@ -4,19 +4,16 @@ import 'bulma'
 import './scss/style.scss'
 import axios from 'axios'
 
+import VoteForm from './components/VoteForm'
+
 
 class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      data: {
-        email: '',
-        candidate: ''
-      },
-      errors: {}
+
     }
-    this.handleChange  = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+
   }
 
   componentDidMount(){
@@ -28,78 +25,12 @@ class App extends React.Component {
       .catch(err =>this.setState({error: err.message}))
   }
 
-  handleChange({target: {name, value}}){
-    const data = {...this.state.data, [name]: value }
-    // reset the errors so when user types it does not see no longer relevant error code
-    this.setState({ data , errors: {}})
-  }
-
-  postVote() {
-    axios
-      .post('/api/vote',
-        this.state.data)
-      .then(this.setState({data: {'candidate': '', 'email': ''}}))
-      .catch(err => this.setState({ errors: err.response.data.errors }))
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-
-    axios
-      //search the db for the email on the form
-      .get(`/api/vote/${this.state.data.email}`)
-      .then(res => {
-        //let this email vote again if it has less than 3 votes
-        if (res.data.length < 3) {
-          this.postVote()
-        } else {
-          // if not reset form and give error message
-          this.setState({errors: { valid: 'This email has already votes 3 times'}, data: {'candidate': '', 'email': ''}})
-        }
-      })
-
-  }
-
   render(){
     if(!this.state) return <h1>Loading...</h1>
     // extracting all the variables to make it more readable
-    const {data: {email, candidate},  errors: {email: errEmail, candidate: errCandidate, valid}} = this.state
 
     return(
-      <section className="section">
-        <div className="container">
-          <form onSubmit={this.handleSubmit}>
-            <div className="field">
-              <div className="control">
-                <input
-                  className="input is-primary"
-                  type="text"
-                  placeholder="Email address"
-                  name="email"
-                  value={email }
-                  onChange={this.handleChange}>
-                </input>
-                {errEmail && <p className="error">{errEmail.message}</p>}
-              </div>
-            </div>
-            <div className="field">
-              <div className="control">
-                <input
-                  className="input is-primary"
-                  type="text"
-                  placeholder="Candidate Number"
-                  name="candidate"
-                  value={candidate}
-                  onChange={this.handleChange}>
-                </input>
-                {errCandidate && <p className="error">{errCandidate.message}</p>}
-              </div>
-            </div>
-            {valid && <p className="error">{valid}</p>}
-            <button  className='button is-primary'>Submit</button>
-          </form>
-        </div>
-      </section>
+      <VoteForm />
     )
   }
 }
